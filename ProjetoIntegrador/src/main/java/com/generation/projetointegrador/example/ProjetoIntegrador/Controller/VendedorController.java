8b9da44a065aba0372e6db400675e12109bfe278
Fernanda <fernandasoares.reis1@gmail.com>
2022-02-02 15:15:10 -0300

    @fernanda-reis [Métodos de Cadastrar e Logar do VendedorController]

--- ProjetoIntegrador/src/main/java/com/generation/projetointegrador/example/ProjetoIntegrador/Controller/VendedorController.java
+++ ProjetoIntegrador/src/main/java/com/generation/projetointegrador/example/ProjetoIntegrador/Controller/VendedorController.java
@@ -1,22 +1,26 @@
 package com.generation.projetointegrador.example.ProjetoIntegrador.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.generation.projetointegrador.example.ProjetoIntegrador.Model.UserLogin;
 import com.generation.projetointegrador.example.ProjetoIntegrador.Model.VendedorModel;
 import com.generation.projetointegrador.example.ProjetoIntegrador.Repository.VendedorRepository;
+import com.generation.projetointegrador.example.ProjetoIntegrador.Service.VendedorService;
 
 @RestController
 @CrossOrigin("*")
@@ -25,6 +29,30 @@ public class VendedorController {
 
 	@Autowired
 	private VendedorRepository repository;
+	
+	@Autowired
+	private VendedorService service;
+		
+	
+	// MÉTODOS GET FIND BY ID / GET FIND ALL
+	
+	
+	
+	@PostMapping("/logar")
+	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user){
+		return service.Logar(user)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());	
+	}
+	
+	@PostMapping("/cadastrar")
+	public ResponseEntity<VendedorModel> Post(@RequestBody VendedorModel user){
+		return service.CadastrarVendedor(user)
+				.map(resp -> ResponseEntity.status(HttpStatus.CREATED).body(resp))
+				.orElseGet(() -> { 
+					throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Vendedor já cadastrado!");
+				});
+	}
 	
 	@GetMapping("/nome/{nomevendedor}")
 	public ResponseEntity<List<VendedorModel>> findByNome(@PathVariable String nomevendedor){
@@ -35,15 +63,7 @@ public class VendedorController {
 			return ResponseEntity.status(HttpStatus.OK).body(list);
 		}
 	}
-		
-	// MÉTODOS GETFINDBY / GETFINDALL
 	
-
-	@PostMapping
-	public ResponseEntity<VendedorModel> postVendedor(@RequestBody VendedorModel nomeVendedor) {
-		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(nomeVendedor));
-	}
-
 	@PutMapping
 	public ResponseEntity<VendedorModel> putVendedor(@RequestBody VendedorModel nomeVendedor) {
 		return ResponseEntity.ok(repository.save(nomeVendedor));
