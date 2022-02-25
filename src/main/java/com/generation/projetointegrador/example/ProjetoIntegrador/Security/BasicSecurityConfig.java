fb6a62c9661df19d2f29593b3a06131e3e8022e6
David <daviidccastro@gmail.com>
2022-02-25 15:07:22 -0300

    @DreAmVID [Alterado rotas BasicSecurity]

--- src/main/java/com/generation/projetointegrador/example/ProjetoIntegrador/Security/BasicSecurityConfig.java
+++ src/main/java/com/generation/projetointegrador/example/ProjetoIntegrador/Security/BasicSecurityConfig.java
@@ -32,8 +32,8 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		.antMatchers("/vendedor/logar").permitAll()
-		.antMatchers("/vendedor/cadastrar").permitAll()
+		.antMatchers("/usuario/logar").permitAll()
+		.antMatchers("/usuario/cadastrar").permitAll()
 		.antMatchers("/produto").permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
