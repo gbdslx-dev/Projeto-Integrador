93ee07ff19a59dfb374d8702a140e4b7d415159e
Fernanda Reis <54251136+fernanda-reis@users.noreply.github.com>
2022-03-09 16:32:16 -0300

    @fernanda-reis [Alteração de permissões security]

--- src/main/java/com/generation/projetointegrador/example/ProjetoIntegrador/Security/BasicSecurityConfig.java
+++ src/main/java/com/generation/projetointegrador/example/ProjetoIntegrador/Security/BasicSecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.http.HttpMethod;
+
 
 @EnableWebSecurity
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
